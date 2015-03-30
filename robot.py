3a9c90a8a4e8880e61a0d548c7ad9e0605bc4917
Amruth Venkatraman <amruthv@gmail.com>
2015-03-30 15:43:38 -0400

    can now move and rotate robot arm around in space

--- robot.py
+++ robot.py
@@ -7,10 +7,7 @@ import math
 class Robot():
     #polygonArr is a array of array of points (not polygon objects or point objects) e.g [[[1,2],[3,4]], [[5,5],[6,6]]] 
     def __init__(self, polygonArr):
-        self.position = [Polygon(polygonPoints) for polygonPoints in polygonArr]
-
-    def getLocation(self):
-        return self.position[0].points[0].arr # first vertex of the first polygon of the robot
+        raise NotImplementedError()
 
     # keep generating them until one is valid
     def generateRandomConfiguration(self):
