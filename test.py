b12178fd7ad72496880ddae3bb8a433551e71c95
Amruth Venkatraman <amruthv@gmail.com>
2015-04-09 21:28:06 -0400

    no idea what changed here...

--- test.py
+++ test.py
@@ -1,170 +1,169 @@
-from linkRobot import LinkRobot
-from polygonalRobot import PolygonalRobot
-from world import World
-from polygonhelper import *
-import pprint
-from collisionchecker import *
+from mcr import MCRPlanner
+from world import World, Obstacle
+from movableLinkRobot import MovableLinkRobot
 from Tkinter import *
 from simulator import *
-from movableLinkRobot import MovableLinkRobot
+import searcher
+import cProfile
 
-
-def testMoveableLinkArm():
+def testNoObstacles():
     master = Tk()
     canvas = Canvas(master, width=500, height=500)
     canvas.pack()
-    sim = Simulator(500, 500)
-    # print canvas.winfo_height()
+    sim = Simulator(canvas, 500, 500)
     world = World(500,500, [])
     links = []
     links.append([0, [(50,50), (90,50), (90,70), (50,70)]])
     links.append([0, [(90,50), (130,50), (130,70), (90,70)]])
     links.append([0, [(130,50), (170,50), (170,70), (130,70)]])
+    start = (50,50, 0, 0, 0)
+    goal = (300, 300, 0, 90, -90)
     linkRobot = MovableLinkRobot(links, world)
-    sim.drawRobot(linkRobot, canvas)
-    sim.drawPoint([50,50], canvas)
-    sim.drawPoint([100,100], canvas)
-    raw_input()
-    canvas.delete("all")
-    linkRobot.moveToConfiguration([100,100, 0, 0, 0])
-    sim.drawRobot(linkRobot, canvas)
-    sim.drawPoint([50,50], canvas)
-    sim.drawPoint([100,100], canvas)
-    raw_input()
-    canvas.delete("all")
-    linkRobot.moveToConfiguration([200,200, 0, 90, -90])
-    sim.drawRobot(linkRobot, canvas)
-    sim.drawPoint([50,50], canvas)
-    sim.drawPoint([100,100], canvas)
-    raw_input()
+    mcr = MCRPlanner(linkRobot, world, start, goal, sim)
+    mcr.discreteMCR()
 
-
-
-
-def test1():
-    links = []
-    world = World(500,500)
-    links.append([0, [(0,0), (4,0), (4,2), (0,2)]])
-    links.append([0, [(4,0), (8,0), (8,2), (4,2)]])
-    links.append([0, [(8,0), (12,0), (12,2), (8,2)]])
-    linkRobot = LinkRobot(links, world, [0,0,0])
-    print 'initial positions'
-    pprint.pprint(linkRobot.position)
-    linkRobot.moveToConfiguration([0,90,-90])
-    print 'after rotations'
-    pprint.pprint(linkRobot.position)
-    linkRobot.moveToConfiguration([0,0,0])
-    print 'testing rotation again back to original flat'
-    pprint.pprint(linkRobot.position)
-
-def test2():
-    world = World(500,500)
-    robotPolygons = []
-    robotPolygons.append([Point(0,0), Point(4,0), Point(4,2), Point(0,2)])
-    robotPolygons.append([Point(4,2), Point(8,2), Point(8,4), Point(4,4)])
-    robot = PolygonalRobot(robotPolygons, world, [0,0,0])
-    print 'initial'
-    print robot.position
-    robot.moveToConfiguration([2,0, 3])
-    print 'rotated'
-    print robot.position
-
-    # master = Tk()
-    # canvas.pack()
-
-def test3():
+def testOneObstacleMiddle():
     master = Tk()
-    canvas = Canvas(master, width=400, height=200)
+    canvas = Canvas(master, width=500, height=500)
     canvas.pack()
+    sim = Simulator(canvas, 500, 500)
 
+    obstacle1 = Obstacle([(200,20), (300,20), (300,150), (200,150)])
+    obstacles = [obstacle1]
+    sim.drawObstacles(obstacles)
+    world = World(500,500, obstacles)
 
-    q_near = [201, 98, 357]
-    q_near = [200, 103, 6]
-    q_goal = [200,100, 0]
-
-    robotPolygon = [Point(20, 80), Point(48,52), Point(76,80), Point(48,108)]
-    obstacle1 = [Point(105,20), Point(185,20), Point(185,60), Point(105,60)]
-    obstacle2 = [Point(100,110), Point(180,110), Point(180,180), Point(100,180)]
-    obstaclesPoints = [obstacle1, obstacle2]
-    obstacles = [Polygon(obstaclePoints) for obstaclePoints in obstaclesPoints]
-    start = [20, 80, 0]
-    goal = [200,100, 0]
-    world = World(400, 200)
-    robot = PolygonalRobot([robotPolygon], world, goal)
-    isParamDegree = [False, False, True]
-    collisionChecker = CollisionChecker(robot, obstacles, isParamDegree)
-    configurations = collisionChecker.buildInBetweenConfigurations(q_near,q_goal)
-    configurations.insert(0, q_near)
-    print configurations
-    for step in configurations:
-        robot.moveToConfiguration(step)
-        canvas.delete("all")
-        drawRobot(robot, canvas)
-        drawObstacles(obstacles, canvas)
-        drawPoint(start, canvas, fill='purple')
-        drawPoint(goal, canvas, fill='cyan')
-        previous = step
-        raw_input()
-    pprint.pprint(configurations)
-
-def test4():
-    master = Tk()
-    canvas = Canvas(master, width=400, height=200)
-    canvas.pack()
-
-    q_rand = [0, 90, -90]
-    q_near = [0.0, 28.2842712474619, -28.2842712474619]
-    print 'q_rand', q_rand
-    print 'q_near', q_near
-
-    obstacle1 = [Point(120,80), Point(160,80), Point(160,100), Point(120,100)]
-    # obstacle2 = [Point(100,110), Point(180,110), Point(180,180), Point(100,180)]
-    obstaclesPoints = [obstacle1]
-    obstacles = [Polygon(obstaclePoints) for obstaclePoints in obstaclesPoints]
-    # obstacles = []
-    start = [0, 0, 0]
-    goal = [0,90, -90]
-    world = World(400, 200)
     links = []
-    links.append([0, [(20,40), (60,40), (60,60), (20,60)]])
-    links.append([0, [(60,40), (100,40), (100,60), (60,60)]])
-    links.append([0, [(100,40), (140,40), (140,60), (100,60)]])
-    linkRobot = LinkRobot(links, world, [0, 90, -90])
-    isParamDegree = [False, False, False]
-    collisionChecker = CollisionChecker(linkRobot, obstacles, isParamDegree)
-    configurations = collisionChecker.buildInBetweenConfigurations(q_near,q_rand)
-    configurations.insert(0, q_near)
-    pprint.pprint(configurations)
-    for step in configurations:
-        linkRobot.moveToConfiguration(step)
-        pprint.pprint('position' + str(linkRobot.position))
-        canvas.delete("all")
-        drawRobot(linkRobot, canvas)
-        drawObstacles(obstacles, canvas)
-        drawPoint(start, canvas, fill='purple')
-        drawPoint(goal, canvas, fill='cyan')
-        previous = step
+    links.append([0, [(50,50), (90,50), (90,70), (50,70)]])
+    links.append([0, [(90,50), (130,50), (130,70), (90,70)]])
+    links.append([0, [(130,50), (170,50), (170,70), (130,70)]])
+    start = (50,50, 0, 0, 0)
+    goal = (320, 50, 0, 90, -90)
+    linkRobot = MovableLinkRobot(links, world)
+    sim.drawRobot(linkRobot)
+    linkRobot.moveToConfiguration(goal)
+    sim.drawRobot(linkRobot)
+    raw_input()
+
+    mcr = MCRPlanner(linkRobot, world, start, goal, sim)
+    sim.drawPoint((start[0], start[1]), fill = 'green')
+    sim.drawPoint((goal[0], goal[1]), fill = 'blue')
+    mcr.discreteMCR()
+    path = searcher.reconstructPath(mcr.cameFrom, goal)
+    drawPath(sim, obstacles, linkRobot, path)
+
+
+def testTwoDiffWeightObstacles():
+    master = Tk()
+    canvas = Canvas(master, width=500, height=500)
+    canvas.pack()
+    sim = Simulator(canvas, 500, 500)
+
+    obstacle1 = Obstacle([(200,20), (300,20), (300,150), (200,150)], 4)
+    obstacle2 = Obstacle([(200,150), (300,150), (300,500), (200,500)], 1)
+    obstacles = [obstacle1, obstacle2]
+    sim.drawObstacles(obstacles)
+    world = World(500,500, obstacles)
+
+    links = []
+    links.append([0, [(50,50), (90,50), (90,70), (50,70)]])
+    links.append([0, [(90,50), (130,50), (130,70), (90,70)]])
+    links.append([0, [(130,50), (170,50), (170,70), (130,70)]])
+    start = (50,50, 0, 0, 0)
+    goal = (320, 50, 0, 90, -90)
+    linkRobot = MovableLinkRobot(links, world)
+    # sim.drawRobot(linkRobot)
+    # linkRobot.moveToConfiguration(goal)
+    # sim.drawRobot(linkRobot)
+    # raw_input()
+    mcr = MCRPlanner(linkRobot, world, start, goal, sim)
+    try:
+        cameFrom = mcr.discreteMCR()
+    except KeyboardInterrupt:
+        drawGraph(sim, obstacles, mcr.G)
+    # path = searcher.reconstructPath(cameFrom, goal)
+    # drawPath(sim, obstacles, linkRobot, path)
+
+def testManyObstacles():
+    master = Tk()
+    canvas = Canvas(master, width=500, height=500)
+    canvas.pack()
+    sim = Simulator(canvas, 500, 500)
+    obstacle1 = Obstacle([(100,0), (275,0), (300,100), (190,180), (75, 100)], 4)
+    obstacle2 = Obstacle([(140, 385), (225,425), (140,480), (55,425)], 1)
+    obstacle3 = Obstacle([(250, 200), (350, 200), (350, 375), (250, 375)])
+    obstacle4 = Obstacle([(375, 90), (480, 90), (480, 180), (375, 180)])
+    obstacle5 = Obstacle([(410, 350), (430, 350), (430, 450), (410, 450), (410, 420), (360, 400), (410, 380)])
+    obstacles = [obstacle1, obstacle2, obstacle3, obstacle4, obstacle5]
+    sim.drawObstacles(obstacles)
+    world = World(500,500, obstacles)
+
+    links = []
+    links.append([0, [(20,200), (60,200), (60,220), (20,220)]])
+    links.append([0, [(60,200), (100,200), (100,220), (60,220)]])
+    links.append([0, [(100,200), (140,200), (140,220), (100,220)]])
+    start = (20,200, 0, 0, 0)
+    goal = (450, 360, 0, 90, 90)
+    linkRobot = MovableLinkRobot(links, world)
+    sim.drawRobot(linkRobot)
+    linkRobot.moveToConfiguration(goal)
+    sim.drawRobot(linkRobot)
+    raw_input()
+    mcr = MCRPlanner(linkRobot, world, start, goal, sim)
+    try:
+        cameFrom = mcr.discreteMCR()
+    except KeyboardInterrupt:
+        drawGraph(sim, obstacles, mcr.G)
+
+
+def blah():
+    master = Tk()
+    canvas = Canvas(master, width=500, height=500)
+    canvas.pack()
+    sim = Simulator(canvas, 500, 500)
+    obstacle1 = Obstacle([(100,0), (275,0), (300,100), (190,180), (75, 100)], 4)
+    obstacle2 = Obstacle([(140, 385), (225,425), (140,480), (55,425)], 1)
+    obstacle3 = Obstacle([(250, 200), (350, 200), (350, 375), (250, 375)])
+    obstacle4 = Obstacle([(375, 90), (480, 90), (480, 180), (375, 180)])
+    obstacle5 = Obstacle([(410, 350), (430, 350), (430, 450), (410, 450), (410, 420), (360, 400), (410, 380)])
+    obstacles = [obstacle1, obstacle2, obstacle3, obstacle4, obstacle5]
+    sim.drawObstacles(obstacles)
+    world = World(500,500, obstacles)
+    sim.drawPoint((138,145))
+    raw_input()
+
+def drawGraph(sim, obstacles, G):
+    sim.clearCanvas()
+    sim.drawObstacles(obstacles)
+    for V in G.V:
+        sim.drawPoint((V[0], V[1]))
+    # for v1 in G.E:
+    #     for v2 in G.E[v1]:
+    #         sim.drawLine(v1[0], v1[1], v2[0], v2[1])
+    raw_input()
+
+def drawPath(sim, obstacles, robot, path):
+    for q in path:
+        sim.clearCanvas()
+        sim.drawObstacles(obstacles)
+        robot.moveToConfiguration(q)
+        sim.drawRobot(robot)
         raw_input()
-    # drawObstacles(obstacles, canvas)
-    # drawRobot(linkRobot, canvas)
-    # # drawPoint(goal, canvas)
-    # raw_input()
-    # linkRobot.moveToConfiguration([0,90,-90])
-    # canvas.delete("all")
-    # isParamDegree = [True, True, True]
-    # drawObstacles(obstacles, canvas)    
-    # drawRobot(linkRobot, canvas)
-    # raw_input()
 
-    # collisionChecker = CollisionChecker(linkRobot, obstacles, isParamDegree)
-    # rrt = RRT(start, 0.1, goal, linkRobot.generateRandomConfiguration, collisionChecker.validateTrajectory)
-    # rrt.growUntilFound()
-    # cameFrom = rrt.cameFrom
+def prof(test, n=50):
+    import cProfile
+    import pstats
+    try:
+        cProfile.run(test, 'prof')
+    except:
+        print 'Done'
+    p = pstats.Stats('prof')
+    p.sort_stats('cumulative').print_stats(n)
 
 
-
-# test1()
-# test2()
-# test3()
-# test4()
-testMoveableLinkArm()
+# testNoObstacles()
+# testOneObstacleMiddle()
+# testTwoDiffWeightObstacles()
+blah()
+# prof('testManyObstacles()')
+# cProfile.run('testTwoDiffWeightObstacles()')
